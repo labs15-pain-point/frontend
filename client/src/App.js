84cba3f23e6eb5c4a2fa30da662759088fcfd1ac
matt0418 <mfeldma7@gmail.com>
2019-09-05 17:28:27 -0400

    set up beginning for about us view

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 // Importing Views
 import LandingView from './views/LandingView'
+import AboutUs from './views/AboutUs'
 
 function App(props) {
   return (
     <div className="App">
       <Route exact path ="/" component={LandingView}/>
+      <Route path='/about' component={AboutUs}/>
     </div>
   );
 }
---
+++ client/src/views/AboutUs.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import AboutMe from '../components/AboutMe'
+
+const teamMembers = [
+    {
+        name: "Michael Bundick",
+        img: '',
+        linkedin: 'https://www.linkedin.com/in/michael-bundick/',
+        github: 'https://github.com/bundickm',
+        about: 'Here is where I would type out an about me'
+
+    },
+    {
+        name: "Daniel Harris",
+        img: '',
+        linkedin: 'https://www.linkedin.com/in/daniel-harris-45a417176/',
+        github: 'https://github.com/veritaem',
+        about: 'Here is where I would type out an about me'
+
+    },
+    {
+        name: "Nicolas Montoya",
+        img: '',
+        linkedin: 'https://www.linkedin.com/in/nico-montoya/',
+        github: 'https://github.com/NicoMontoya',
+        about: 'Here is where I would type out an about me'
+
+    },
+    {
+        name: "Tomas Fox",
+        img: '',
+        linkedin: 'https://www.linkedin.com/in/tomasfox1/',
+        github: 'https://github.com/tomfox1',
+        about: 'Here is where I would type out an about me'
+
+    },
+    {
+        name: "Matthew Feldman",
+        img: '',
+        linkedin: 'https://www.linkedin.com/in/matthew-r-feldman/',
+        github: 'https://github.com/matt0418',
+        about: 'Here is where I would type out an about me'
+    }
+]
+
+const AboutUs = props => {
+    return(
+        <div> 
+            {teamMembers.map(member => {
+                return <AboutMe member={member} />
+            })}
+        </div>
+       
+    )
+    
+}
+
+export default AboutUs
