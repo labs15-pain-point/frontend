3db622ace33cff5bbcafe6fe7436fb645cb418f9
matt0418 <mfeldma7@gmail.com>
2019-09-05 16:51:21 -0400

    added redirects and netlify settings

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
+// Importing Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
+
+
 
 import './index.css';
 import App from './App';
