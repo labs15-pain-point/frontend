1f3fd5ef5dd0661301c3bdb4d6fb94ec297ae9be
veritaem <dcharris776@gmail.com>
2019-09-05 19:14:59 -0400

    leaderboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import './App.css';
 // Importing Views
 import LandingView from './views/LandingView'
 import AboutUs from './views/AboutUs'
+import LeaderboardView from './views/LeaderboardView'
 
 function App(props) {
   return (
     <div className="App">
       <Route exact path ="/" component={LandingView}/>
       <Route path='/about' component={AboutUs}/>
+      <Route path ='/leaderboard' component={LeaderboardView}/>
     </div>
   );
 }
