814ef31b6b48d3192c04c506e032287a06c1478f
bundickm <bundickm@gmail.com>
2019-09-05 15:43:17 -0700

    Added a how it works view to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 
 // Importing Views
 import LandingView from './views/LandingView'
+import HowItWorksView from './views/HowItWorksView'
 
 function App(props) {
   return (
     <div className="App">
       <Route exact path ="/" component={LandingView}/>
+      <Route path = "/howitworks" component={HowItWorksView}/>
     </div>
   );
 }
---
+++ client/src/components/HowItWorksFuture.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HowItWorksFuture = props => {
+    return (
+        <div>
+            <h1>The Future</h1>
+            <p>
+                You’ll notice that some of the random ideas we serve up after all this filtering are still nonsense while other company descriptions sound rather legitimate. We still have some crud, which is where this site comes in. By upvoting and downvoting company descriptions you help us label and filter descriptions so that future iterations of the project will be even better. 
+            </p>
+            <p>
+                Don’t want to wait for our next product cycle? Head over to the leaderboard to see what’s already made it to the top.
+            </p>
+        </div>
+    )
+}
+
+export default HowItWorksFuture
